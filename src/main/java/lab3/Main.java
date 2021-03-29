3a89a979e5bdee1ca6e75cc5cfb8c9a446e63fc1
Egor Ostryakov <eg.ostr@mail.ru>
2021-03-29 16:43:05 +0300

    add handler for methods

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -2,8 +2,8 @@ package lab3;
 
 import lab3.function.Answer;
 import lab3.function.Functions;
-import lab3.method.SolvingMethod;
-import lab3.method.TrapeziumMethod;
+import lab3.method.*;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.InputMismatchException;
@@ -11,11 +11,18 @@ import java.util.Scanner;
 
 @Slf4j
 public class Main {
-    private static SolvingMethod method = new TrapeziumMethod();
+    private static final String commandFormat = "CM_Lab3 [-lrmts] [-i file_path] [-o file_path]\n" +
+            "-l Метод прямоугольников (левый)\n" +
+            "-r Метод прямоугольников (правый)\n" +
+            "-m Метод прямоугольников (средний)\n" +
+            "-t Метод трапеций\n" +
+            "-s Метод Симпсона";
+    private static SolvingMethod method;
     private static Functions function;
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
+            configure(args);
             chooseFunction(scanner);
             chooseLimits(scanner);
             System.out.println("Введите эпсилон:");
@@ -28,6 +35,32 @@ public class Main {
             System.err.println("Введённые данные некоректны");
         } catch (Exception e) {
             System.err.println(e.getMessage());
+        }
+    }
+
+    @SneakyThrows
+    private static void configure(String[] args) {
+        if (args.length != 1)
+            throw new RuntimeException("Неверное количество аргументов\n" + commandFormat);
+
+        switch (args[0]) {
+            case "-l":
+                method = new RectangleLeftMethod();
+                break;
+            case "-m":
+                method = new RectangleMiddleMethod();
+                break;
+            case "-r":
+                method = new RectangleRightMethod();
+                break;
+            case "-t":
+                method = new TrapeziumMethod();
+                break;
+            case "-s":
+                method = new SimpsonMethod();
+                break;
+            default:
+                throw new RuntimeException("Неверный формат команды\n" + commandFormat);
         }
     }
 
--- src/main/java/lab3/method/SolvingMethod.java
+++ src/main/java/lab3/method/SolvingMethod.java
@@ -3,6 +3,29 @@ package lab3.method;
 import lab3.function.Answer;
 import lab3.function.Functions;
 
+import java.util.function.Function;
+
 public interface SolvingMethod {
-    Answer solve(Functions functions);
+    default Answer solve(Functions functions) {
+        int n = 4;
+        double a = functions.getLeft(), b = functions.getRight();
+        Function<Double, Double> function = functions.getFunction();
+        boolean reverse = false;
+        if (a > b) {
+            a = functions.getRight();
+            b = functions.getLeft();
+            reverse = true;
+        }
+        double I0, I1;
+        while (true) {
+            I0 = getIntegral(n, a, b, function);
+            I1 = getIntegral(n * 2, a, b, function);
+            if (Math.abs(I1 - I0) < functions.getEpsilon()) break;
+            n *= 2;
+        }
+        I1 = reverse ? -I1 : I1;
+        return new Answer(I1, n);
+    }
+
+    double getIntegral(int i, double a, double b, Function<Double, Double> function);
 }
